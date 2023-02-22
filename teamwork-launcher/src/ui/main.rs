8a695973651d3d6657c1b02a82be511aa7a8656b
irabeson <iohannrabeson@users.noreply.github.com>
2023-02-22 16:38:30 -0500

    Fix to Iced 0.8.0

--- teamwork-launcher/src/ui/main.rs
+++ teamwork-launcher/src/ui/main.rs
@@ -1,3 +1,4 @@
+use iced::Length;
 use iced_spinner::spinner;
 use {
     crate::{
@@ -16,7 +17,7 @@ use {
     iced::{
         theme,
         widget::{column, container, horizontal_space, pane_grid, row, scrollable, text, toggler, Container, PaneGrid},
-        Alignment, Element, Length,
+        Alignment, Element,
     },
     iced_lazy::responsive,
 };
