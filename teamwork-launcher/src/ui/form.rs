2edf3e3a5507d39f52e11ee7db5b894d8f5d130d
IohannRabeson <IohannRabeson@users.noreply.github.com>
2023-03-12 17:15:32 -0400

    Hardcode the width of the labels in the server details view.
    This hack resolve #110, but it would be better to have a grid layout or a form layout.
    
    close #110

--- teamwork-launcher/src/ui/form.rs
+++ teamwork-launcher/src/ui/form.rs
@@ -1,7 +1,4 @@
-use iced::{
-    widget::{column, row, text},
-    Element, Padding,
-};
+use iced::{widget::{column, row, text}, Element, Padding, Length};
 
 type FieldFn<'l, T, Message> = Box<dyn Fn(&'l T) -> Element<'l, Message> + 'l>;
 
@@ -49,17 +46,8 @@ impl<'l, T, Message: 'l> Form<'l, T, Message> {
     }
 
     pub fn view(&self, context: &'l T) -> Element<'l, Message> {
-        let labels = self
-            .fields
-            .iter()
-            .fold(column![], |column, (label, _)| column.push(text(&label)))
-            .spacing(self.spacing);
-        let fields = self
-            .fields
-            .iter()
-            .fold(column![], |column, (_, field_fn)| column.push((field_fn)(context)))
-            .spacing(self.spacing);
-
-        row![labels, fields].spacing(self.spacing).padding(self.padding).into()
+        self.fields.iter().fold(column![], |column, (label, field_fn)| {
+            column.push(row![text(label).width(Length::Fixed(160.0)), (field_fn)(context)])
+        }).into()
     }
 }
