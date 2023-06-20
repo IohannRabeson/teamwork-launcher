8b6f6e1806f960a7cb68c88e70b044144b790f1a
Iohann Rabeson <IohannRabeson@users.noreply.github.com>
2023-06-19 23:35:09 -0400

    Do not download archives when testing. (#180)
    * Do not download archives when testing.
    Instead the archives are now stored in the repository so they will not change.

--- mods-manager/src/lib.rs
+++ mods-manager/src/lib.rs
@@ -10,3 +10,17 @@ pub use {
     reqwest::Url,
     source::{fetch_package, FetchError, Source},
 };
+
+#[cfg(test)]
+mod tests {
+    use std::path::{Path, PathBuf};
+
+    pub fn get_resource_path(relative_path: impl AsRef<Path>) -> PathBuf {
+        let mut p = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+
+        p.push("resources");
+        p.push("tests");
+        p.push(relative_path);
+        p
+    }
+}
